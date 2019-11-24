b1bb43a2c1d9819e655ecea12e8aae6011cd8d3f
omar <crespomerchano@gmail.com>
2019-11-24 15:48:58 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JackCres C0de - JS Avanzado
+
+You must need to put a video in mp4 format called source.mp4 in the assets folder
---
+++ main.js
@@ -0,0 +1,3 @@
+const video = document.querySelector('video')
+
+video.onclick = () => video.paused ? video.play() : video.pause()
